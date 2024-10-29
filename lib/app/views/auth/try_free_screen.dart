b402efcf6d023fe1d6f570b8ecf568ea608ea12d
CodeK254 <codekarma1003@gmail.com>
2024-10-29 09:11:03 +0300

    v0.1.3

---
+++ lib/app/views/auth/try_free_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TryFreeScreen extends StatelessWidget {
+  const TryFreeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
