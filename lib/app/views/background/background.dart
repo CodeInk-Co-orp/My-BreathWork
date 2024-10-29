d83a00d24df5074bfb00176de58392a8c9446180
CodeK254 <codekarma1003@gmail.com>
2024-10-29 07:04:32 +0300

    v0.0.8

--- lib/app/views/background/background.dart
+++ lib/app/views/background/background.dart
@@ -1,6 +1,6 @@
-import 'package:animated_background/animated_background.dart';
 import 'package:flutter/material.dart';
 import 'package:my_breath_work/app/widgets/space.dart';
+import 'package:my_breath_work/util/colors.dart';
 
 class BackgroundScreen extends StatefulWidget {
   const BackgroundScreen({super.key});
@@ -13,21 +13,13 @@ class _BackgroundScreenState extends State<BackgroundScreen> with SingleTickerPr
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedBackground(
-        vsync: this, 
-        behaviour: RandomParticleBehaviour(
-          options: ParticleOptions(
-            spawnMaxSpeed: 25,
-            particleCount: int.parse((verticalSpace(context, .01).floor() * horizontalSpace(context, 1).floor() ~/ 5).toString()),
-            spawnMinSpeed: 15,
-            spawnOpacity: .75,
-            baseColor: Colors.white,
-          ),
-        ),
-        child: Container(
-          height: verticalSpace(context, 1),
-          width: horizontalSpace(context, 1),
-          color: Colors.black,
+      body: Container(
+        height: verticalSpace(context, 1),
+        width: horizontalSpace(context, 1),
+        color: KColors.black,
+        child: const Image(
+          image: AssetImage('assets/images/background_animated.gif'),
+          fit: BoxFit.cover,
         ),
       ),
     );
