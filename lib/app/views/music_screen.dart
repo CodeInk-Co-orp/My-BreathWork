1ad7537b62a2e7668d66d7521393222e1a256824
CodeK254 <codekarma1003@gmail.com>
2024-11-07 18:32:55 +0300

    Choose Screen Modification

--- lib/app/views/music_screen.dart
+++ lib/app/views/music_screen.dart
@@ -9,6 +9,7 @@ import 'package:my_breath_work/app/widgets/home_row.dart';
 import 'package:my_breath_work/app/widgets/space.dart';
 import 'package:my_breath_work/app/widgets/text.dart';
 import 'package:my_breath_work/util/colors.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class MusicScreen extends StatelessWidget {
   final MusicController musicController = Get.put(MusicController());
@@ -17,201 +18,203 @@ class MusicScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BackgroundScreen(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: verticalSpace(context, 0.3),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage(
-                    "assets/audio_image.jpg",
+      body: Skeletonizer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: verticalSpace(context, 0.3),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage(
+                      "assets/audio_image.jpg",
+                    ),
+                    fit: BoxFit.cover,
                   ),
-                  fit: BoxFit.cover,
+                  borderRadius: BorderRadius.circular(5),
                 ),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: const Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: HomeRow(),
-                ),
-              ),
-            ),
-            Slider(
-              value: musicController.sliderValue.value, 
-                onChanged: (value){
-              }
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 16,
-                right: 16
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomText(
-                    text: "00.00", 
-                    fontSize: 15, 
-                    textColor: KColors.white
+                child: const Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: HomeRow(),
                   ),
-                  CustomText(
-                    text: "04.39", 
-                    fontSize: 15, 
-                    textColor: KColors.white
-                  ),
-                ],
+                ),
               ),
-            ),
-            const CustomText(
-              text: "Be happy - space by Elle ", 
-              fontSize: 20, 
-              textColor: KColors.white,
-              fontWeight: FontWeight.w800,
-            ),
-            SizedBox(
-              height: verticalSpace(context, 0.02),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.skip_next_sharp,
-                  color: KColors.white,
-                  size: 35,
-                ),
-                SizedBox(width: 12,),
-                Icon(
-                  CupertinoIcons.play_circle_fill,
-                  color: KColors.white,
-                  size: 35,
-                ),
-                SizedBox(width: 12,),
-                Icon(
-                  Icons.skip_next_sharp,
-                  color: KColors.white,
-                  size: 35,
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: horizontalSpace(context, 0.03)
+              Slider(
+                value: musicController.sliderValue.value, 
+                  onChanged: (value){
+                }
               ),
-              child: Row(
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomText(
+                      text: "00.00", 
+                      fontSize: 15, 
+                      textColor: KColors.white
+                    ),
+                    CustomText(
+                      text: "04.39", 
+                      fontSize: 15, 
+                      textColor: KColors.white
+                    ),
+                  ],
+                ),
+              ),
+              const CustomText(
+                text: "Be happy - space by Elle ", 
+                fontSize: 20, 
+                textColor: KColors.white,
+                fontWeight: FontWeight.w800,
+              ),
+              SizedBox(
+                height: verticalSpace(context, 0.02),
+              ),
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Slider(
-                    value: musicController.volume.value,
-                    max: 10,
-                    min: 0,
-                    onChanged: (value){
-                    }
-                  ),
-                  const Icon(
-                    Icons.volume_up_sharp,
+                  Icon(
+                    Icons.skip_next_sharp,
                     color: KColors.white,
-                    size: 28,
-                  )
+                    size: 35,
+                  ),
+                  SizedBox(width: 12,),
+                  Icon(
+                    CupertinoIcons.play_circle_fill,
+                    color: KColors.white,
+                    size: 35,
+                  ),
+                  SizedBox(width: 12,),
+                  Icon(
+                    Icons.skip_next_sharp,
+                    color: KColors.white,
+                    size: 35,
+                  ),
                 ],
               ),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CircleAvatar(
-                  child: Icon(
-                    Icons.music_note_sharp,
-                    size: 28,
-                  ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: horizontalSpace(context, 0.03)
                 ),
-                CircleAvatar(
-                  child: Icon(
-                    Icons.mic_external_on_outlined,
-                    size: 28,
-                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Slider(
+                      value: musicController.volume.value,
+                      max: 10,
+                      min: 0,
+                      onChanged: (value){
+                      }
+                    ),
+                    const Icon(
+                      Icons.volume_up_sharp,
+                      color: KColors.white,
+                      size: 28,
+                    )
+                  ],
                 ),
-                CircleAvatar(
-                  child: Icon(
-                    CupertinoIcons.speaker,
-                    size: 28,
-                  ),
-                ),
-                CircleAvatar(
-                  child: Icon(
-                    Icons.record_voice_over_sharp,
-                    size: 28,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: verticalSpace(context, 0.03),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center, 
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Transform.rotate(
-                    angle: -pi /2,
-                    child: SizedBox(
-                      width: 110,
-                      child: Slider(
-                        value: musicController.mix1.value,
-                        onChanged: (value) {
-                        },
-                        max: 10,
-                        min: 0,
-                      ),
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.music_note_sharp,
+                      size: 28,
                     ),
                   ),
-                  Transform.rotate(
-                    angle: -pi / 2,
-                    child: SizedBox(
-                      width: 110,
-                      child: Slider(
-                        value: musicController.mix2.value,
-                        onChanged: (value) {
-                        },
-                        max: 10,
-                        min: 0,
-                      ),
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.mic_external_on_outlined,
+                      size: 28,
                     ),
                   ),
-                  Transform.rotate(
-                    angle: -pi / 2,
-                    child: SizedBox(
-                      width: 110,
-                      child: Slider(
-                        value: musicController.mix3.value,
-                        onChanged: (value) {
-                        },
-                        max: 10,
-                        min: 0,
-                      ),
+                  CircleAvatar(
+                    child: Icon(
+                      CupertinoIcons.speaker,
+                      size: 28,
                     ),
                   ),
-                  Transform.rotate(
-                    angle: -pi / 2,
-                    child: SizedBox(
-                      width: 110,
-                      child: Slider(
-                        value: musicController.mix4.value,
-                        onChanged: (value) {
-                        },
-                        max: 10,
-                        min: 0,
-                       ),
-                      ),
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.record_voice_over_sharp,
+                      size: 28,
+                    ),
                   ),
-               ],
-            ),
-            SizedBox(
-              height: verticalSpace(context, 0.03),
-            ),
-          ],
+                ],
+              ),
+              SizedBox(
+                height: verticalSpace(context, 0.03),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center, 
+                  children: [
+                    Transform.rotate(
+                      angle: -pi /2,
+                      child: SizedBox(
+                        width: 110,
+                        child: Slider(
+                          value: musicController.mix1.value,
+                          onChanged: (value) {
+                          },
+                          max: 10,
+                          min: 0,
+                        ),
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: -pi / 2,
+                      child: SizedBox(
+                        width: 110,
+                        child: Slider(
+                          value: musicController.mix2.value,
+                          onChanged: (value) {
+                          },
+                          max: 10,
+                          min: 0,
+                        ),
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: -pi / 2,
+                      child: SizedBox(
+                        width: 110,
+                        child: Slider(
+                          value: musicController.mix3.value,
+                          onChanged: (value) {
+                          },
+                          max: 10,
+                          min: 0,
+                        ),
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: -pi / 2,
+                      child: SizedBox(
+                        width: 110,
+                        child: Slider(
+                          value: musicController.mix4.value,
+                          onChanged: (value) {
+                          },
+                          max: 10,
+                          min: 0,
+                         ),
+                        ),
+                    ),
+                 ],
+              ),
+              SizedBox(
+                height: verticalSpace(context, 0.03),
+              ),
+            ],
+          ),
         ),
       ),
     );
